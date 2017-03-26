ff68b699314e1041abd2a582197d290ef7182282
DzMS <dzigmihail@gmail.com>
2017-03-26 21:38:10 +0300

    changed DBQuerry

--- src/ua/dzms/database/DBQuery.java
+++ src/ua/dzms/database/DBQuery.java
@@ -1,40 +1,61 @@
 package ua.dzms.database;
 
-import com.mysql.jdbc.*;
+import com.mysql.jdbc.Connection;
 
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class DBQuery {
 
     public DBQuery(String query) throws SQLException {
-        Connection dbConnection = getConnection();
-        Statement statement = (Statement) dbConnection.createStatement();
-        StringBuilder answerDB = new StringBuilder();
+        executeQuery(query);
+    }
 
-        if (statement.execute(query)) {
-            ResultSet resultSet = statement.executeQuery(query);
+    private void executeQuery(String query) {
+        Connection dbConnection = getConnection();
+        try {
+            Statement statement = dbConnection.createStatement();
+            if (statement.execute(query)) {
+                print(statement.getResultSet());
+
+            } else {
+                System.out.print("You query is done");
+            }
+            statement.close();
+            dbConnection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private Connection getConnection() {
+        Connection connection = null;
+        try {
+            connection = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb?",
+                    "root", "admin");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+    private void print(ResultSet resultSet) {
+        StringBuilder answerDB = new StringBuilder();
+        try {
             while (resultSet.next()) {
                 int count = resultSet.getMetaData().getColumnCount();
                 for (int i = 1; i <= count; i++) {
                     String temp = resultSet.getMetaData().getColumnName(i);
-                    answerDB.append(temp + "=" + resultSet.getString(temp) + ", ");
+                    answerDB.append(temp);
+                    answerDB.append(" = ");
+                    answerDB.append(resultSet.getString(temp));
+                    answerDB.append(", ");
                 }
                 answerDB.append("\n");
             }
-            resultSet.close();
-        }else {
-            answerDB.append("All is OK");
+            System.out.print(answerDB);
+        }catch (SQLException e){
+            e.printStackTrace();
         }
-        statement.close();
-        dbConnection.close();
-        System.out.print(answerDB);
-
-    }
-
-    private Connection getConnection() throws SQLException {
-        return (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb?useSSL=false",
-                "root", "admin");
     }
 }
